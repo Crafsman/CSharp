f6ffd399265ba473389af4898290db5a2a12a52a
Mingqian Yang <yang@xtom.com>
2018-10-24 12:13:28 +1100

    add three models for Library ManagementCourse Project

---
+++ aspCore/LibraryManagementCourse/LibraryManagementCourse/Data/Model/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementCourse.Data.Model
+{
+    public class Customer
+    {
+    }
+}
