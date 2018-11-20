00264e8f3f996e79969764c8553a0bfa264f9378
Mingqian Yang <yang@xtom.com>
2018-11-20 18:24:25 +1100

    Start up 'DrinkAndRun' project

---
+++ aspCore/test/test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter unit price:");
+                string price = Console.ReadLine();
+
+                Regex reg = new Regex(@"^\d+(\.\d\d)?$");
+                if (reg.IsMatch(price))
+                //if (Regex.IsMatch(price, @"^(-)?\d+(\.\d\d)?$"))
+                {
+                    Console.WriteLine("Valid price!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid");
+                }
+            }
+
+
+
+        }
+    }
+}
